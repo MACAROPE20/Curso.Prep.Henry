38a4e15558176e318a57587791c3bde905892a9b
MACAROPE20 <camila.peralta1620@gmail.com>
2022-02-08 21:03:39 -0500

    Tarea-Terminada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -167,6 +167,8 @@ function numeroRandom() {
   //Pista: investigá qué hace el método Math.random()
   return Math.random();
 }
+numeroRandom(8);
+
 function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
@@ -196,16 +198,16 @@ function combinarNombres(nombre, apellido) {
   // Tu código:
   return nombre + apellido;
 }
-combinarNombres('camila'||'peralta'); 
+combinarNombres('camila'||,'peralta'); 
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'hola' + nombre + '!'; 
+  return 'Hola' + nombre + '!'; 
   
 }
-obtenerSaludo('Nicolas'); 
+obtenerSaludo('Martin'); 
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
@@ -245,6 +247,8 @@ function esVocal(letra){
   //Escribe tu código aquí
   if (letra === 'a'|| letra === 'e'|| letra === 'i'||letra === 'o'|| letra === 'u'){
     return 'Es vocal”';
+  }else if (!letra === 'a'|| letra === 'e'|| letra === 'i'||letra === 'o'|| letra === 'u'){
+    return 'Dato incorrecto';
   }else {
     return 'Dato incorrecto';
   }
